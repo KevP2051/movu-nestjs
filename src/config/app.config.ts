45e089c301ecf27ec2dae24096df32e25ba798f9
Kevin Poveda <kevinpovedaj@gmail.com>
2026-01-25 15:52:29 -0500

    feat: add new .env variables and validations for jwt implementation

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,7 +2,9 @@
 export const EnvConfiguration = () => ({
     app:{
         port:process.env.PORT,
-        environment:process.env.ENVIRONMENT
+        environment:process.env.ENVIRONMENT,
+        jwtSecret:process.env.JWT_SECRET,
+        jwtExpiresIn:process.env.JWT_EXPIRES_IN
     },
     database:{
         host:process.env.DB_HOST,
