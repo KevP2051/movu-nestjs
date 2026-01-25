1e25a2c83f792532403bca867984abcd934cd886
Kevin Poveda <kevinpovedaj@gmail.com>
2026-01-25 18:33:45 -0500

    feat: add sendWelcomeEmail method

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { NodemailerAdapter } from './adapters/node-mailer-adapter';
 
 @Injectable()
 export class EmailService {
  
+  constructor(
+    private readonly emailSenderAdapter:NodemailerAdapter,
+  ) {}
 
-  send
+
+  async sendWelcomeEmail(to: string, name: string) {
+    const html = `
+      <div style="font-family: Arial, sans-serif;">
+        <h2>¡Bienvenido, ${name}!</h2>
+        <p>Gracias por registrarte en nuestra aplicación.</p>
+        <p>¡Esperamos que disfrutes la experiencia!</p>
+      </div>
+    `;
+    await this.emailSenderAdapter.sendMail({
+      to,
+      subject: '¡Bienvenido a la app!',
+      html,
+    });
+  }
+  
 }
