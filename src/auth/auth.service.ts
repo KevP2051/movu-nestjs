58311ef2e9e277e5380a42d92e2a4ceaa8cdef80
Kevin Poveda <kevinpovedaj@gmail.com>
2026-01-25 16:39:17 -0500

    feat: implement jwt signing method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,19 +3,25 @@ import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { LoginUserDto } from './dto';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
 export class AuthService {
 
-  constructor(private readonly usersService: UsersService) {
+  constructor(private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {
   }
 
 
 
   async registerUser(createUserDto: CreateUserDto) {
 
-    return await this.usersService.create(createUserDto);
+    const user = await this.usersService.create(createUserDto);
+
+    return {user, token: this.getJwtToken(user!.id)};
+
 
   }
 
@@ -29,15 +35,24 @@ export class AuthService {
       throw new BadRequestException('Credentials are not valid');
     }
 
-    if (bcrypt.compareSync(password, user.password)) {
-
-
-
+    if(!bcrypt.compareSync(password, user.password)){
+      throw new BadRequestException('Credentials are not valid');
     }
+ 
+
+    const { password: passwordToDelete, ...userWithoutPassword } = user;
+
+    return{
+      user: userWithoutPassword,
+      token: this.getJwtToken(user.id)
+    }
+
   }
 
-  signJwt(id: string) {
-    
+  getJwtToken(id:string){
+   return this.jwtService.sign({
+      id: id
+    });
   }
 
 }
