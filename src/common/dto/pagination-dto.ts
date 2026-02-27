e8c62d45b268bee1230beaa08f2be01cc3fe7a98
Kevin Poveda <kevinpovedaj@gmail.com>
2026-02-27 18:28:23 -0500

    feat: create paginationdto in common module

---
+++ src/common/dto/pagination-dto.ts
@@ -0,0 +1,16 @@
+import { IsNumber, IsOptional, IsPositive } from "class-validator";
+
+export class PaginationDto {
+
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    offset: number;
+
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    limit: number;
+}
