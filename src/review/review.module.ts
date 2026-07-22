b248c92268f8023a6a6cc65b82742f1c57af2ff0
Kevin Poveda <kevinpovedaj@gmail.com>
2026-07-21 21:02:19 -0500

    feat: integrate TypeORM for Review entity in review module and service

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
+import { Review } from './entities/review.entity';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 
 @Module({
   controllers: [ReviewController],
   providers: [ReviewService],
+  imports: [TypeOrmModule.forFeature([Review])],
 })
-export class ReviewModule {}
+export class ReviewModule { }
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Review } from './entities/review.entity';
 
 @Injectable()
 export class ReviewService {
+
+  constructor(
+    @InjectRepository(Review)
+    private readonly reviewRepository: Review
+  ) { }
+
+
   create(createReviewDto: CreateReviewDto, userId: string) {
+
+    try {
+
+
+
+    } catch (error) {
+
+    }
     return 'This action adds a new review';
   }
 
